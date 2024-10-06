55e246173feab8306b25c1f9c6778b1ac823199d
PabloGMD <pablogmdsoftware@gmail.com>
2024-10-06 13:50:17 +0200

    Change the naming style of the classes

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
-const buttonList = document.querySelectorAll(".chrbutton");
+const buttonList = document.querySelectorAll(".characterButton");
 
 console.log(buttonList);
 
 buttonList.forEach(buttonElement => {
     console.log("n");
     buttonElement.addEventListener("click", () => {
-        if (buttonElement.classList.contains("button-clicked")) {
-            buttonElement.classList.remove("button-clicked");
+        if (buttonElement.classList.contains("button--clicked")) {
+            buttonElement.classList.remove("button--clicked");
         } else {
-            buttonElement.classList.add("button-clicked");
+            buttonElement.classList.add("button--clicked");
         }
         console.log("Something here");
     });
