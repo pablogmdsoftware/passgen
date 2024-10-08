48d948c640a6078169ba1b47c2c4375820fe9804
PabloGMD <pablogmdsoftware@gmail.com>
2024-10-08 11:33:12 +0200

    Restructure initApp

--- js/main.js
+++ js/main.js
@@ -45,38 +45,40 @@ document.addEventListener("readystatechange", (event) => {
 });
 
 const initApp = () => {
-    const addSet = new Set();
-    const buttonList = document.querySelectorAll(".characterButton");
 
-    buttonList.forEach(buttonElement => {
-        buttonElement.addEventListener("click", () => {
-            if (buttonElement.classList.contains("button--clicked")) {
-                buttonElement.classList.remove("button--clicked");
-                addSet.delete(buttonElement.textContent);
-            } else {
-                buttonElement.classList.add("button--clicked");
-                addSet.add(buttonElement.textContent);
-            };
-            console.log(addSet);
-        });
+const addSet = new Set();
+const buttonList = document.querySelectorAll(".characterButton");
+
+buttonList.forEach(buttonElement => {
+    buttonElement.addEventListener("click", () => {
+        if (buttonElement.classList.contains("button--clicked")) {
+            buttonElement.classList.remove("button--clicked");
+            addSet.delete(buttonElement.textContent);
+        } else {
+            buttonElement.classList.add("button--clicked");
+            addSet.add(buttonElement.textContent);
+        };
+        console.log(addSet);
     });
+});
 
-    const pw = document.getElementsByClassName("password")[0];
-    const buttonGenerate = document.getElementsByClassName("buttonGenerate")[0];
-    const copyButton = document.getElementsByClassName("copyButton")[0];
+const pw = document.getElementsByClassName("password")[0];
+const buttonGenerate = document.getElementsByClassName("buttonGenerate")[0];
+const copyButton = document.getElementsByClassName("copyButton")[0];
 
-    buttonGenerate.addEventListener("click", () => {
-        pw.value = passgen2(lengthInput.value,addSet);
-    });
+buttonGenerate.addEventListener("click", () => {
+    pw.value = passgen2(lengthInput.value,addSet);
+});
 
-    copyButton.addEventListener("click", () => {
-        navigator.clipboard.writeText(pw.value);
-    })
+copyButton.addEventListener("click", () => {
+    navigator.clipboard.writeText(pw.value);
+})
 
-    const lengthInput = document.getElementById("lengthInput");
-    const lengthNumber = document.getElementById("lengthNumber");
+const lengthInput = document.getElementById("lengthInput");
+const lengthNumber = document.getElementById("lengthNumber");
 
-    lengthInput.oninput = (() => {
-        lengthNumber.textContent = lengthInput.value;
-    });
+lengthInput.oninput = (() => {
+    lengthNumber.textContent = lengthInput.value;
+});
+
 }
